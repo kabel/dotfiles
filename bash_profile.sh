a38ba2ba67db568fe3caa9343d662875b5c945c1
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-07 13:36:30 -0600

    Fix bash completion from homebrew bash-completion@2
    Sets BASH_COMPLETION_COMPAT_DIR to continue supporting git completion

--- bash_profile.sh
+++ bash_profile.sh
@@ -33,7 +33,7 @@ shopt -s cdspell
 # Bash completion
 [ -f /etc/profile.d/bash-completion ] && source /etc/profile.d/bash-completion
 [ -f "$HOMEBREW_PREFIX/etc/bash_completion" ] && source "$HOMEBREW_PREFIX/etc/bash_completion" >/dev/null
-[ -f "$HOMEBREW_PREFIX/share/bash-completion/bash_completion" ] && source "$HOMEBREW_PREFIX//share/bash-completion/bash_completion" >/dev/null
+[ -f "$HOMEBREW_PREFIX/share/bash-completion/bash_completion" ] && export BASH_COMPLETION_COMPAT_DIR="$HOMEBREW_PREFIX/etc/bash_completion.d" && source "$HOMEBREW_PREFIX/share/bash-completion/bash_completion" >/dev/null
 [ -f "$HOME/.composer/vendor/stecman/composer-bash-completion-plugin/hooks/bash-completion" ] && source "$HOME/.composer/vendor/stecman/composer-bash-completion-plugin/hooks/bash-completion" >/dev/null
 
 # Colorful prompt
