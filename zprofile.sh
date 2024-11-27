bf6c3bc1bd52d00db0d49735588fbb8f3081296c
Kevin Abel <kevin.abel@hudl.com>
2024-11-27 11:01:52 -0600

    Add orbstack integration to z profile

--- zprofile.sh
+++ zprofile.sh
@@ -8,3 +8,7 @@ source ~/.shprofile
 # Enable history appending instead of overwriting.
 setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY
+
+# Added by OrbStack: command-line tools and integration
+# Comment this line if you don't want it to be added again.
+source ~/.orbstack/shell/init.zsh 2>/dev/null || :
